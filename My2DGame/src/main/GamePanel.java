a1db9b76ccac9dba150cbb63e5ff2528caf32536
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-08-30 14:08:17 -0600

    Screen Creation In Progress
    2D Game Ep1 To Continue

---
+++ My2DGame/src/main/GamePanel.java
@@ -0,0 +1,27 @@
+﻿package main;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+//this class extends JPanel which means its a subclass of JPanel
+public class GamePanel extends JPanel {
+    //screen settings
+//final	A non-access modifier used for classes, attributes and methods, which makes them non-changeable (impossible to inherit or override)
+    final int originalTitleSize = 16; //16x16 title;
+    final int scale = 3; // upscaling the size 16x3(scale) = 48
+
+    final int titleSize = originalTitleSize * scale; // 48x48 tile
+    final int maxScreenCol = 16; // 16 tiles horizontally
+    final int maxScreenRow = 12; // 12 tiles vertically
+    final int screenWidth = titleSize * maxScreenCol; //  48x16 =  768px
+    final int screenHeight = titleSize * maxScreenRow; // 48x12 = 576pixels
+
+    //constructor
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+
+}
---
+++ My2DGame/src/main/Main.java
@@ -0,0 +1,24 @@
+﻿package main;
+
+import javax.swing.*;
+
+public class Main {
+    //without static void main a java program cannot be run
+    public static void main(String[] args) {
+        //to create a window
+        JFrame window = new JFrame();
+        //frame gets closed when exited
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //cannot be resized windows
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        //windows will be displayed at the center of the screen
+        window.setLocationRelativeTo(null);
+        //windows will be visible
+        window.setVisible(true);
+    }
+}
